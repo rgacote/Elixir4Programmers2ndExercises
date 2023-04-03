6e01e1371fa4d274470e857653de563cbd452afb
Raymond GA Côté <rgacote@AppropriateSolutions.com>
2023-04-03 15:57:04 -0400

    Start Coding

---
+++ hangman/dictionary/lib/dictionary.ex
@@ -0,0 +1,18 @@
+defmodule Dictionary do
+  @moduledoc """
+  Documentation for `Dictionary`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Dictionary.hello()
+      :ok
+
+  """
+  def hello do
+    IO.puts("!!Hello world!!")
+  end
+end
