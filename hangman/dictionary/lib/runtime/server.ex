657bc59ccd55562c8a5136841e1f2d76e6fc6eba
Raymond GA Côté <rgacote@AppropriateSolutions.com>
2023-04-18 09:52:40 -0400

    Supervisor

--- hangman/dictionary/lib/runtime/server.ex
+++ hangman/dictionary/lib/runtime/server.ex
@@ -4,11 +4,18 @@ defmodule Dictionary.Runtime.Server do
 
   @me __MODULE__
 
-  def start_link() do
+  use Agent
+
+  def start_link(_) do
     Agent.start_link(&WordList.word_list/0, name: @me)
   end
 
   def random_word() do
+    # Randomly crash agent.
+    if :rand.uniform() < 0.2 do
+      Agent.get(@me, fn _ -> exit(:boom) end)
+    end
+
     Agent.get(@me, &WordList.random_word/1)
   end
 end
