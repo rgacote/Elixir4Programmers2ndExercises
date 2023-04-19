5cca86a08c974bffa847100320b0d6d6ce5ff6e9
Raymond GA Côté <rgacote@AppropriateSolutions.com>
2023-04-19 09:57:12 -0400

    Update Text Client to new API

--- hangman/text_client/lib/impl/player.ex
+++ hangman/text_client/lib/impl/player.ex
@@ -24,8 +24,8 @@ defmodule TextClient.Impl.Player do
     IO.puts(feedback_for(tally))
     IO.puts(current_word(tally))
 
-    Hangman.make_move(game, get_guess())
-    |> interact()
+    tally = Hangman.make_move(game, get_guess())
+    interact({game, tally})
   end
 
   @spec feedback_for(tally) :: String.t()
